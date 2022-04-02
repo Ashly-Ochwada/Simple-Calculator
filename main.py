d76c69a423d6621662390e1cbbb6cc3bd4daa6f2
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-04-02 20:16:11 +0300

    calculator

---
+++ main.py
@@ -0,0 +1,23 @@
+#adds two numbers
+def add(x,y):
+    return x+y
+
+#subtracts two numbers
+def subtract(x,y):
+    return x-y
+
+#multiplies two numbers
+def multiply (x,y):
+    return x*y
+
+#divides two numbers
+def divide (x,y):
+    return x/y
+
+
+
+    print("select operation.")
+    print("Add")
+    print("Subtract")
+    print("Multiply")
+    print("Divide")   
